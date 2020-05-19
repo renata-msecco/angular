a54148b870fea872a14a7b62e5ec850f5c104037
Renata Secco <renata.secco@gmail.com>
2020-05-19 17:53:09 -0300

    feat: entendendo life cycle hooks

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnChanges, OnInit, AfterViewInit {
 
   @Input() title = 'Seja Bem Vindx';
+  constructor(){
+    console.log('constructor');
+  }
+  ngOnChanges() {
+    console.log('ngOnChanges');
+  }
+  ngOnInit(): void{
+  console.log('ngOnInit');
+  }
+ ngAfterViewInit(){
+ console.log('ngAfterViewInit');
+ }
 }
+
