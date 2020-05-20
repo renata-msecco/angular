79cb429c964891e7c8fbf783d770beaf37d7905a
Renata Secco <renata.secco@gmail.com>
2020-05-20 14:33:06 -0300

    chore: reorganizando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 //import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { ExercicioDataBindingComponent } from './exercicio-data-binding/exercicio-data-binding.component';
-import { ExercicioContadorComponent } from './exercicio-contador/exercicio-contador.component';
-import { ExercicioDiretivasComponent } from './exercicio-diretivas/exercicio-diretivas.component';
-import { ExercicioNgclassComponent } from './exercicio-ngclass/exercicio-ngclass.component';
-import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { ExercicioDataBindingComponent } from './shared/components/exercicio-data-binding/exercicio-data-binding.component';
+import { ExercicioContadorComponent } from './shared/components/exercicio-contador/exercicio-contador.component';
+import { ExercicioDiretivasComponent } from './shared/components/exercicio-diretivas/exercicio-diretivas.component';
+import { ExercicioNgclassComponent } from './shared/components/exercicio-ngclass/exercicio-ngclass.component';
+import { ExercicioPipesComponent } from './shared/components/exercicio-pipes/exercicio-pipes.component';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
-import { ExercicioPipesLocaleidComponent } from './exercicio-pipes-localeid/exercicio-pipes-localeid.component';
+import { ExercicioPipesLocaleidComponent } from './shared/components/exercicio-pipes-localeid/exercicio-pipes-localeid.component';
 registerLocaleData(localePt, 'pt');
 
 @NgModule({
