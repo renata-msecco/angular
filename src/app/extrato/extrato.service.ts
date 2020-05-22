49e4d7dcb6126dcbbe4c1fbde90401e5cb3c63af
Renata Secco <renata.secco@gmail.com>
2020-05-22 11:09:18 -0300

    feat: variaveis de ambiente

--- src/app/extrato/extrato.service.ts
+++ src/app/extrato/extrato.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Transacao } from './extrato.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExtratoService {
 
+  API_URL = environment.API_URL;
+
   constructor(
     private http: HttpClient
   ) { }
 
   getTransacoes() {
-    return this.http.get<Transacao[]>('https://my-json-server.typicode.com/renata-msecco/myapi/transacoes');
+    return this.http.get<Transacao[]>(this.API_URL + '/transacoes');
+    // return this.http.get<Transacao[]>(`${this.API_URL}/transacoes`);
+   // notação com template string.
   }
 }
